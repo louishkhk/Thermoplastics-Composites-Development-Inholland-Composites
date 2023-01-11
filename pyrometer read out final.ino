8a8272a56f9f5575c1e1ce632e599c155a191b48
louishkhk <122457315+louishkhk@users.noreply.github.com>
2023-01-11 15:12:34 +0100

    Update pyrometer read out final.ino

--- pyrometer read out final.ino
+++ pyrometer read out final.ino
@@ -1,8 +1,8 @@
 #include "max6675.h"
 
-int thermoPyroDO = 5;
+int thermoPyroDO = 5; //choose pin numbers 
 int thermoPyroCS = 6;
-int thermoPyroCLK = 7; //arrange pins in master code
+int thermoPyroCLK = 7; 
 MAX6675 thermocouplePyro(thermoPyroCLK, thermoPyroCS, thermoPyroDO);
 
 float laminatetemp = 0;
@@ -18,6 +18,5 @@ void loop() {
    
   Serial.print("1C = "); 
   Serial.println(thermocouplePyro.readCelsius());// code to read out temp
-
   laminatetemp = thermocouplePyro.readCelsius();
 }
